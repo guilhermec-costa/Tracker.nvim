40774c3baa5255a44cca2e9320090b7aacbac584
Guilherme China <echina725@gmail.com>
2024-04-30 12:37:25 -0300

    chore(tables): more about  tables

--- references/tables.lua
+++ references/tables.lua
@@ -1,12 +1,22 @@
 local table_x = {
     keyx = "valuex",
     ["keyy"] = "valuey",
-    [5] = "value5"
+    [5] = "value5",
+    [7] = tostring(true),
+    func = P(function()
+        return nil
+    end)
 }
 
 local array = { 1, 2, 3, 4, 5 }
 
 for key, value in pairs(table_x) do
+    print("key: " .. key .. " | value: " .. value)
+end
+
+print('-----------------------')
+
+for key, value in ipairs(table_x) do
     print("key: " .. key .. " | value: " .. value)
 end
 
@@ -22,6 +32,27 @@ for index, value in ipairs(array) do
     print("key: " .. index .. " | value: " .. value * 2)
 end
 
-table.insert(table_x, 4, "churros")
-table.sort(table_x)
+for i, _ in ipairs(table_x) do
+    print(i)
+end
+
+print('-----------------------')
+
+table_x["<leader>C"] = "A keyboard"
+P(table_x)
+
+print('-----------------------')
+
+for key in pairs(table_x) do
+    print("Key: " .. key)
+end
+
+print('-----------------------')
+
+for key, value in ipairs({ 1, 2 ,3}) do
+    print("Index: " .. key)
+end
+
+
+table_x["Global"] = "Global"
 P(table_x)
